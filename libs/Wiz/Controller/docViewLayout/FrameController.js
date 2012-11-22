43e8912d0d80c523563d2f0dfee99b1aa0b3eba8
rechie1985 <lsl1276@gmail.com>
2012-11-22 12:44:06 +0800

    修改文档阅读区域resize不正常的bug

--- libs/Wiz/Controller/docViewLayout/FrameController.js
+++ libs/Wiz/Controller/docViewLayout/FrameController.js
@@ -19,14 +19,16 @@ define(function (require, exports, module) {
 		 * @return {[type]}        [description]
 		 */
 		function resizeFrameContainer(iframe){
-	    if (iframe) {               
-	      if (iframe.contentDocument && iframe.contentDocument.body.offsetHeight) {
-	        iframe.parentElement.style.height = iframe.contentDocument.documentElement.scrollHeight+20 + 'px'; 
-	        iframe.parentElement.style.width = iframe.contentDocument.documentElement.scrollWidth + 20 + 'px';
-	      }
-	      else if (iframe.Document && iframe.Document.body.scrollHeight){
-	        iframe.parentElement.style.height = iframe.Document.documentElement.scrollHeight+20 + 'px';    
-	        iframe.parentElement.style.width = iframe.Document.documentElement.scrollWidth + 20 + 'px';               
+			console.log('iframe resize');
+	    if (iframe) {
+	    	var fDoc = (iframe.contentDocument) ? iframe.contentDocument : iframe.contentWindow.document,//兼容firefox和ie
+	    			fDocElem = fDoc.documentElement,
+	    			parentStyle = iframe.parentElement.style;
+	      if (fDoc && fDocElem.scrollHeight && fDocElem.scrollWidth) {
+	      	//首先清空
+	      	parentStyle.height = parentStyle.width = '';
+	        parentStyle.height = fDocElem.scrollHeight + 20 + 'px'; 
+	        parentStyle.width = fDocElem.scrollWidth + 20 + 'px';
 	      }
 	    }
 		}
