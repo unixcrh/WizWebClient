3627f00ede8315c3ae7ab05fe6cee8be205758b4
rechie1985 <lsl1276@gmail.com>
2013-01-09 16:11:34 +0800

    增加对ajax请求的控制，保证同一个api的请求同一时间只能有一个

--- web/libs/Wiz/remote.js
+++ web/libs/Wiz/remote.js
@@ -3,7 +3,8 @@ define(["./context","./constant","../component/loading"], function(require, expo
 	var context = require('./context'),
 			constant = require('./constant'),
 			loadCtrl = require('../component/loading'),
-			bPostDocLock = false;
+			bPostDocLock = false,
+			ajaxCtrl = new AjaxControl();
 
 	//发送请求函数
 	//options主要是处理url后衔接的objValue，如document_title、category_name...
@@ -35,10 +36,14 @@ define(["./context","./constant","../component/loading"], function(require, expo
 					loadCtrl.hide();
 				}
 				bPostDocLock = false;
+				if ('abort' === status) {
+					// 手动停掉的不做任何
+					return;
+				}
 				callError(error);
 			},
 			url = options ? (apiObj.url + '/' + options) : apiObj.url;
-		$.ajax({
+		var jqxhr = $.ajax({
 			url: url,
 			data: data,
 			dateType: 'json',
@@ -48,6 +53,8 @@ define(["./context","./constant","../component/loading"], function(require, expo
 			error: _callError,
 			cache: false
 		});
+		// 
+		ajaxCtrl.setCurrent(url + apiObj.action, jqxhr);
 	}
 
 	//获取请求参数
@@ -77,7 +84,58 @@ define(["./context","./constant","../component/loading"], function(require, expo
 		}
 		return targetParams;
 	}
+	
+	/**
+	 * 对ajax进行控制，同一类型的ajax请求同时只能有一个，其他的必须要abort
+	 */
+	function AjaxControl() {
+		var xhrListObj = {};
 
+		function abortSame (key) {
+			var xhrList = xhrListObj[key],
+					length = 0,
+					i = 0;
+			if (xhrList) {
+				length = xhrList.length;
+				for (i=length ; i>0; i--) {
+					try {
+						// 取消
+						xhrList[i].abort();
+						// 删除
+						xhrList.splice(i-1, 1);
+
+					} catch (error) {
+						// 暂时不做任何操作
+					}
+				}
+			}
+		}
+
+		function abortAll () {
+			if (!xhrListObj) {
+				return;
+			}
+			for (var key in xhrListObj) {
+				abortSame(key);
+			}
+		}
+
+		function setCurrent (key, jqxhr) {
+			abortSame(key);
+			xhrListObj[key] = xhrListObj[key] || [];
+			xhrListObj[key].push(jqxhr);
+		}
+
+		return {
+			setCurrent: setCurrent,
+			abortAll: abortAll
+		};
+
+	}
+	/**
+	 * 远程调用的类
+	 * @type {Object}
+	 */
 	var Remote = {
 
 		/* 获取用户信息 */
