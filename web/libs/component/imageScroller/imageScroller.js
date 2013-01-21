a146c5f1e8a3e1ce662a3cb656a6025453c13cf9
rechie1985 <lsl1276@gmail.com>
2013-01-21 20:10:58 +0800

    增加左右调节按钮

--- web/libs/component/imageScroller/imageScroller.js
+++ web/libs/component/imageScroller/imageScroller.js
@@ -16,7 +16,7 @@
 	},
 	_data = {
 		itemList: [],
-		startIndex: -1,
+		startIndex: 0,
 		endIndex: -1,
 		totalCount: -1
 	}
@@ -48,7 +48,7 @@
 				+ '<img class="Thumb" src="https://a.gfx.ms//txt_57.png" style="padding-top: 52px;"></div>',
 			overlayLinkHTML = '<a href="#" class="FloatRight"><img src="https://a.gfx.ms/liveview_download.png"></a>';
 	var ScrollBar = function() {
-
+		var operateBtn = null;
 		/**
 		 * 初始化函数，唯一接口	
 		 * @param  {[Array]} config [ ]
@@ -60,17 +60,16 @@
 			setItemList(config.items);
 			bindHeaderClickHandler();
 			showByContainer();
+			addOperateBtn();
 		}
 
 		function showByContainer() {
-			_data.endIndex = _data.pageCount = getPageCount();
-			console.log(_data.endIndex);
+			_data.endIndex = _data.pageCount = getNumPerPage();
 			show(_data.startIndex, _data.endIndex);
 		}
 
-		function getPageCount() {
+		function getNumPerPage() {
 			var containerWidth = mediasContainer.width();
-			console.log(containerWidth);
 			var itemWidth = 130;
 			var pageCount = Math.ceil(containerWidth/itemWidth);
 			return pageCount;
@@ -84,6 +83,8 @@
 		 * @return {[type]}            [description]
 		 */
 		function show(beginIndex, endIndex) {
+			console.log('beginIndex: ' + beginIndex);
+			console.log('endIndex: ' + endIndex);
 			var curList = _data.itemList.slice(beginIndex, endIndex),
 				length = curList.length;
 
@@ -91,30 +92,28 @@
 				mediasContainer.append(curList[i]);	
 			}
 		}
+		function addOperateBtn() {
+			operateBtn = new NavButton(_data.itemList.length, getNumPerPage(), show);
+			operateBtn.insertInto(mediasContainer);
+		}
 
 
 		function setItemList(itemList) {
 			if (_data.itemList !== []) {
-				 removeCurList();
+				 reset();
 			}
 			if(itemList) {
 				var length = itemList.length;
 				for (var i=0; i<length; i++) {
 					var itemElem = createItemAndBind(itemList[i], i);
 					_data.itemList.push(itemElem);
-					mediasContainer.append(itemElem);
 				}
+				showByContainer();
 			}
-		}
-
-		/**
-		 * 清除当前显示的列表
-		 * @return {[type]} [description]
-		 */
-		function removeCurList() {
-			reset();
-			mediasContainer.html('');
-			console.log(mediasContainer);
+			// 每次更新list的时候，必须要更新btn的状态
+			if (operateBtn !== null) {
+				operateBtn.record(_data.itemList.length, getNumPerPage());
+			}
 		}
 
 		function reset() {
@@ -124,6 +123,7 @@
 				endIndex: -1,
 				totalCount: -1
 			};
+			mediasContainer.html('');
 		}
 
 		function bindHeaderClickHandler() {
@@ -150,9 +150,7 @@
 
 		// 根据配置的linkElem信息确定位置
 		function initToLinkElem(linkElem) {
-			console.log(linkElem);
 			if (linkElem) {
-				console.log('insertAfter');
 				$(outerHTML).insertAfter($(linkElem));
 			} else {
 				$(document.body).append(outerHTML);
@@ -203,5 +201,114 @@
 			setTitle: setTitle
 		}
 	};
+
+
+	var NavButton = function (sum, numPerPage, showFunction) {
+		this.record(sum, numPerPage);
+		this.jqPrevBtnElem = $('<a/>').addClass('NavButton Prev').hide();
+		this.jqNextBtnElem = $('<a/>').addClass('NavButton Next').hide();
+		this.show = showFunction;
+		this.init();
+	};
+	NavButton.prototype.init = function () {
+		this.initEventHandler();
+		this.changeView();
+	};
+	NavButton.prototype.record = function(sum, numPerPage) {
+		this.sum = sum;
+		this.numPerPage = numPerPage;
+		this.pageCount = Math.ceil(sum/numPerPage);
+		this.curPageNum = 1;
+	};
+
+	NavButton.prototype.initEventHandler = function() {
+		var self = this;
+		self.jqPrevBtnElem[0].onmousedown = function() {
+			self.jqPrevBtnElem.addClass('Press');
+		};
+		self.jqNextBtnElem[0].onmousedown = function() {
+			self.jqNextBtnElem.addClass('Press');
+		};
+		self.jqPrevBtnElem[0].onmouseup = function() {
+			self.jqPrevBtnElem.removeClass('Press');
+		};
+		self.jqNextBtnElem[0].onmouseup = function() {
+			self.jqNextBtnElem.removeClass('Press');
+		};
+		self.jqPrevBtnElem[0].onmouseover = function() {
+			self.jqPrevBtnElem.addClass('Hover');
+		};
+		self.jqNextBtnElem[0].onmouseover = function() {
+			self.jqNextBtnElem.addClass('Hover');
+		};
+		self.jqPrevBtnElem[0].onmouseout = function() {
+			self.jqPrevBtnElem.removeClass('Hover');
+		};
+		self.jqNextBtnElem[0].onmouseout = function() {
+			self.jqNextBtnElem.removeClass('Hover');
+		};
+		self.jqPrevBtnElem[0].onclick = function() {
+			self.showPrev();
+		};
+		self.jqNextBtnElem[0].onclick = function() {
+			self.showNext();
+		};
+	};
+	NavButton.prototype.insertInto = function(jqContainer) {
+		jqContainer.append(this.jqPrevBtnElem);
+		jqContainer.append(this.jqNextBtnElem );
+	};
+
+	NavButton.prototype.showNext = function () {
+		console.log('pageCount:' + this.pageCount);
+		console.log('curPageNum:' + this.curPageNum);
+		console.log('sum:' + this.sum);
+		console.log('numPerPage:' + this.numPerPage);
+		if(this.pageCount <= 1 || this.curPageNum === this.pageCount) {
+			return;
+		}
+		this.curPageNum += this.curPageNum;
+		if (this.curPageNum === this.pageCount) {
+			this.jqNextBtnElem.hide();
+		}
+		this.show((this.curPageNum - 1) * this.numPerPage, this.curPageNum * this.numPerPage);
+		this.changeView();
+	};
+	NavButton.prototype.showPrev = function () {
+		console.log('pageCount:' + this.pageCount);
+		console.log('curPageNum:' + this.curPageNum);
+		console.log('sum:' + this.sum);
+		console.log('numPerPage:' + this.numPerPage);
+		if (this.pageCount <= 1 || this.curPageNum === 1) {
+			return;
+		}
+		this.curPageNum -= this.curPageNum;
+		if (this.curPageNum === 1) {
+			this.jqPrevBtnElem.hide();
+		}
+		this.show((this.curPageNum - 1) * this.numPerPage, this.curPageNum * this.numPerPage);
+		this.changeView();
+	};
+
+	NavButton.prototype.changeView = function() {
+		console.log('pageCount:' + this.pageCount);
+		console.log('curPageNum:' + this.curPageNum);
+		console.log('sum:' + this.sum);
+		console.log('numPerPage:' + this.numPerPage);
+		if (this.pageCount === 1) {
+			this.jqNextBtnElem.hide();
+			this.jqPrevBtnElem.hide();
+		} else if (this.curPageNum === 1) {
+			this.jqNextBtnElem.show();
+			this.jqPrevBtnElem.hide();
+		} else if (this.curPageNum === this.pageCount) {
+			this.jqNextBtnElem.hide();
+			this.jqPrevBtnElem.show();
+		} else {
+			this.jqNextBtnElem.show();
+			this.jqPrevBtnElem.show();
+		}
+	};
+
 	$.fn.scrollbar = new ScrollBar();
 })(jQuery);
