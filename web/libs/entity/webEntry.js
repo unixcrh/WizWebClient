8c687965b9a44d3adf626e688561bdb25a45304e
rechie1985 <lsl1276@gmail.com>
2013-01-22 13:53:53 +0800

    修复oepra

--- web/libs/entity/webEntry.js
+++ web/libs/entity/webEntry.js
@@ -1,3 +1,7 @@
+var localeLanguage = (navigator.language || navigator.browserLanguage).toLowerCase();
+if (localeLanguage !== 'zh-cn' && localeLanguage !== 'zh-tw') {
+	localeLanguage = '';
+}
 seajs.config({
 	// alias: {
 	// 	'common': '/libs/common',																//通用库
@@ -9,8 +13,8 @@ seajs.config({
 	map: [
 	// 时间戳控制版本管理
     [ /^(.*\/libs\/.*\.(?:css|js))(?:.*)$/i, '$1?201212251828' ]
-  ]
+  ],
 
-	// locale: navigator.language.toLowerCase()										//默认语言包，根据系统语言决定
+	locale: 'zh-cn'								//默认语言包，根据系统语言决定
 });
 seajs.use(['seajs/plugins/plugin-i18n', '/web/libs/entity/web-main']);//'./libs/entity/web-main');
